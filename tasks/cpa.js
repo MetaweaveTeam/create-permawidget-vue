0ba5a9c0c4f1a90c5ccc11b7f5b8438ea221a0a6
André Nunes <hello@anuness.dev>
2022-11-15 11:32:22 +0000

    Fix copy function

--- tasks/cpa.js
+++ tasks/cpa.js
@@ -28,6 +28,42 @@ try {
   }
 }
 
+function copyFileSync(source, target) {
+  var targetFile = target;
+
+  // If target is a directory, a new file with the same name will be created
+  if (fs.existsSync(target)) {
+    if (fs.lstatSync(target).isDirectory()) {
+      targetFile = path.join(target, path.basename(source));
+    }
+  }
+
+  fs.writeFileSync(targetFile, fs.readFileSync(source));
+}
+
+function copyFolderRecursiveSync(source, target) {
+  var files = [];
+
+  // Check if folder needs to be created or integrated
+  var targetFolder = path.join(target, path.basename(source));
+  if (!fs.existsSync(targetFolder)) {
+    fs.mkdirSync(targetFolder);
+  }
+
+  // Copy
+  if (fs.lstatSync(source).isDirectory()) {
+    files = fs.readdirSync(source);
+    files.forEach(function (file) {
+      var curSource = path.join(source, file);
+      if (fs.lstatSync(curSource).isDirectory()) {
+        copyFolderRecursiveSync(curSource, targetFolder);
+      } else {
+        copyFileSync(curSource, targetFolder);
+      }
+    });
+  }
+}
+
 async function main() {
   try {
     console.log("Downloading files...");
@@ -40,7 +76,19 @@ async function main() {
 
     console.log("Removing useless files");
     execSync("npx rimraf ./.git");
-    fs.rmdirSync(path.join(projectPath, "tasks"), { recursive: true });
+    execSync("npx rimraf ./package.json");
+    execSync("npx rimraf ./package-lock.json");
+    execSync("npx rimraf ./.gitignore");
+    execSync("npx rimraf ./tasks");
+
+    console.log("Copying Template to root");
+    // copy everything from template to root
+    execSync(
+      "mv ./template/* . || " +
+        "move ./template/* . || " +
+        "xcopy ./template/* . /s /e /y || " +
+        "cp -r ./template/* . "
+    );
 
     console.log("The installation is done, this is ready to use !");
   } catch (error) {
